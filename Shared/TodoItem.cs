93cbabb775836933546ce22291bdb6d31a72db42
kkentt <kenhtet@gmail.com>
2020-06-25 12:06:33 +0630

    Working title pass version

---
+++ Shared/TodoItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Shared
+{
+    public class TodoItemModel
+    {
+        public string Task { get; set; }
+        public bool IsDone { get; set; }
+    }
+
+    public class TodoCardModel
+    {
+       public string Title { get; set; }
+       public List<TodoItemModel> Card { get; set; }
+    }
+
+
+
+}
