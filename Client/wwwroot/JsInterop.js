0ef502c42c7a1141a28d033ff74c654e2c93ec6d
kkentt <kenhtet@gmail.com>
2020-06-30 16:19:48 +0630

    latest

---
+++ Client/wwwroot/JsInterop.js
@@ -0,0 +1,10 @@
+﻿window.UIJsLibraryFunctions =
+{
+    activateFreeWall: function (id) {
+        //var usableId = "#" + id;
+        var wall = new Freewall("#container");
+        wall.fitWidth();
+        console.log("freewall");
+        //alert("test");
+    }
+}
