7c7841ed0ae04ef9047e144bbe5d587720c286e2
kkentt <kenhtet@gmail.com>
2020-07-08 10:20:56 +0630

    Revert "ddd"
    This reverts commit d1a473fb85d79efff3739ed12d5dab1bc06d3997.

--- Client/wwwroot/JsInterop.js
+++ Client/wwwroot/JsInterop.js
@@ -1,19 +1,16 @@
 ﻿window.UIJsLibraryFunctions =
 {
-    //activateFreeWall: function (id) {
-    //    //var usableId = "#" + id;
-    //    var wall = new Freewall("#container");
-    //    wall.fitWidth();
-    //    //alert("test");
-    //}
-    activateMagicGrid: function ()
-    {
-        let magicGrid = new MagicGrid({
-            container: "#container", // Required. Can be a class, id, or an HTMLElement.
-            static: true, // Required for static content.
-            animate: true, // Optional.
-        });
+    activateFreeWall: function (id) {
+        //var usableId = "#" + id;
+        var wall = new Freewall("#container");
+        wall.fitWidth();
+        wall.fitHeight();
 
-        magicGrid.listen();
+        wall.reset({
+            selector: '.brick',
+            draggable: true,
+            animate: true,
+        });
+        //alert("test");
     }
 }
