d32ff283963436e35746a0bf96857a9251374602
kkentt <kenhtet@gmail.com>
2020-07-16 22:43:06 +0630

    Card color

--- Client/wwwroot/JsInterop.js
+++ Client/wwwroot/JsInterop.js
@@ -6,14 +6,15 @@
     //    wall.fitWidth();
     //    //alert("test");
     //}
-    activateMagicGrid: function ()
-    {
+    activateMagicGrid: function () {
         let magicGrid = new MagicGrid({
             container: "#container", // Required. Can be a class, id, or an HTMLElement.
             static: true, // Required for static content.
             animate: true, // Optional.
         });
-
         magicGrid.listen();
+    },
+    openColorPicker: function () {
+            $("#colorItem").click();
     }
 }
