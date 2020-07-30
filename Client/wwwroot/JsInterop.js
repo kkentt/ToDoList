431d80b2f1b924671da5fd2114349b7ef5e27f7a
kkentt <kenhtet@gmail.com>
2020-07-31 00:04:02 +0630

    color change based on background color

--- Client/wwwroot/JsInterop.js
+++ Client/wwwroot/JsInterop.js
@@ -15,11 +15,23 @@
             var container = colorInput.closest('div.item'); 
             colorInput.addEventListener("input",  (event) => {
                 container.style.background = event.target.value;
+                var result = getContrastYIQ(event.target.value);
+                //container.style.color = result;
+                container.style.setProperty("color", result , "important");
             }, false);
         });
+
+        function getContrastYIQ(hexcolor) {
+            hexcolor = hexcolor.replace("#", "");
+            var r = parseInt(hexcolor.substr(0, 2), 16);
+            var g = parseInt(hexcolor.substr(2, 2), 16);
+            var b = parseInt(hexcolor.substr(4, 2), 16);
+            var yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+            return (yiq >= 128) ? 'black' : 'white';
+        }
     },
     openColorPicker: function (colorPickerId) {
         var id = "#" + colorPickerId;
         $(id).click();
-    }
+    },
 }
