cfa7f6b198bd08c2694deb51da5b24e2f370198d
kkentt <kenhtet@gmail.com>
2020-08-17 22:09:24 +0630

    Before changing foreach cardlist to for i loop to put index number for delete

--- Client/wwwroot/JsInterop.js
+++ Client/wwwroot/JsInterop.js
@@ -2,7 +2,7 @@
 {
     activateMagicGrid: function () {
         let magicGrid = new MagicGrid({
-            container: "#container", // Required. Can be a class, id, or an HTMLElement.
+            container: "#items-wrapper", // Required. Can be a class, id, or an HTMLElement.
             static: true, // Required for static content.
             animate: true, // Optional.
         });
