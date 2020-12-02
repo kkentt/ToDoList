8eed3537f75cee63bd80bdaee5f71f87579c038a
kkentt <kenhtet@gmail.com>
2020-12-02 21:45:25 +0630

    Delete no need pages.

---
+++ Client/AppState.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Client
+{
+    public class AppState
+    {
+        //public BackgroundColorModel bgColorModel = new BackgroundColorModel();
+        public string DefaultColor { get; set; } = "#b3dfc4";
+        public bool IsRandomColored { get; private set; } = true;
+        public event Action<bool> StateChanged;
+        private void NotifyStateChanged(bool Property)
+        {
+            StateChanged?.Invoke(Property);
+        }
+
+        public void SetIsRandomColored(bool value)
+        {
+            IsRandomColored = value;
+            NotifyStateChanged(IsRandomColored);
+        }
+
+        public async Task<string> GetRandomColor()
+        {
+            var random = new Random();
+            var color = String.Format("#{0:X6}", random.Next(0x1000000)); // = "#A197B9"
+            return color;
+        }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using ToDoList.Client.Shared;
 
 namespace ToDoList.Client
 {
@@ -18,6 +19,8 @@ namespace ToDoList.Client
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            //builder.Services.AddSingleton<MainLayout>();
+            builder.Services.AddScoped<AppState>();
 
             await builder.Build().RunAsync();
         }
